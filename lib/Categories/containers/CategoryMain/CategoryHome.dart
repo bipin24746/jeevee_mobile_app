43440441899ee49e3f885237bbf63d57f9c5b2a8
bipin24746 <sainjubipin247460@gmail.com>
2024-11-24 21:19:10 +0545

    first commit

---
+++ lib/Categories/containers/CategoryMain/CategoryHome.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:jeevee_mobile_app/Categories/containers/BabyFoods/baby_foods.dart';
+import 'package:jeevee_mobile_app/Categories/containers/BabyFormula/baby_formula.dart';
+import 'package:jeevee_mobile_app/Categories/containers/CategoryMain/category_List.dart';
+import 'package:jeevee_mobile_app/Categories/containers/Cleansers/cleansers.dart';
+import 'package:jeevee_mobile_app/Categories/containers/Conditioners/conditioners.dart';
+import 'package:jeevee_mobile_app/Categories/containers/Diapers/diapers.dart';
+import 'package:jeevee_mobile_app/Categories/containers/FaceMoisturizer/face_moisturizers.dart';
+import 'package:jeevee_mobile_app/Categories/containers/FaceSunscreen/face_sunscreen.dart';
+import 'package:jeevee_mobile_app/Categories/containers/FaceWash/facewash.dart';
+import 'package:jeevee_mobile_app/Categories/containers/FeedingBottleBowls/feeding_bottlebowls.dart';
+import 'package:jeevee_mobile_app/Categories/containers/Gels/gels.dart';
+import 'package:jeevee_mobile_app/Categories/containers/HairCreams&Masks/haircreams.dart';
+import 'package:jeevee_mobile_app/Categories/containers/HairOil/hairoil.dart';
+import 'package:jeevee_mobile_app/Categories/containers/HairSerum/hairSerum.dart';
+import 'package:jeevee_mobile_app/Categories/containers/Kits&Combo/kitsCombo.dart';
+import 'package:jeevee_mobile_app/Categories/containers/Lotions&Creams/lotionCreams.dart';
+import 'package:jeevee_mobile_app/Categories/containers/PersonalCare/personal_care.dart';
+import 'package:jeevee_mobile_app/Categories/containers/Shampoo/shampoo.dart';
+import 'package:jeevee_mobile_app/Categories/containers/Wipes&Buds/wipesBuds.dart';
+import 'package:jeevee_mobile_app/Categories/containers/WomensFragrances/womenFragrances.dart';
+import 'package:jeevee_mobile_app/Categories/containers/mensFragrances/menFragrances.dart';
+import 'package:jeevee_mobile_app/Categories/containers/serums_essence/categorySerums.dart';
+import 'package:jeevee_mobile_app/Navbar/navbar.dart';
+
+class CategoryMainPage extends StatefulWidget {
+  const CategoryMainPage({super.key});
+
+  @override
+  State<CategoryMainPage> createState() => _CategoryMainPageState();
+}
+
+class _CategoryMainPageState extends State<CategoryMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Navbar(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            MakeUpList(),
+            SerumsEssence(),
+            PersonalCare(),
+            FaceSunscreen(),
+            FaceMoisturizers(),
+            Facewash(),
+            Lotioncreams(),
+            Cleansers(),
+            Haircreams(),
+            Shampoo(),
+            Womenfragrances(),
+            Menfragrances(),
+            Kitscombo(),
+            Hairoil(),
+            Hairserum(),
+            Conditioners(),
+            Diapers(),
+            BabyFormula(),
+            Wipesbuds(),
+            BabyFoods(),
+            FeedingBottlebowls(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Categories/containers/CategoryMain/category_List.dart
@@ -0,0 +1,376 @@
+import 'package:flutter/material.dart';
+
+class MakeUpList extends StatelessWidget {
+  const MakeUpList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 8, right: 4),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Skincare",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 4, right: 8),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Makeup",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 8, right: 4),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Mom & Baby",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 4, right: 8),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Kid's Fashion",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 8, right: 4),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Health & Wellness",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 4, right: 8),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Personal Care",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 8, right: 4),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Haircare",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 4, right: 8),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Fragrances",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 8, right: 4),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Gadgets",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 4, right: 8),
+                  child: SizedBox(
+                    height: 100,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                          image:
+                              AssetImage('lib/assets/images/smartphones.jpg'),
+                          fit: BoxFit.cover, // Ensures the image covers the box
+                        ),
+                      ),
+                      child: Container(
+                        color: Colors.black
+                            .withOpacity(0.4), // Semi-transparent overlay
+                        child: Center(
+                          child: Text(
+                            "Accessories",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("WHAT DO YOU HAVE "),
+              Text(
+                "CONCERN WITH?",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 40,
+          ),
+        ],
+      ),
+    );
+  }
+}
